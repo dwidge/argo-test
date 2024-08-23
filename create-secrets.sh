b5e53cf249fc3117c5ebb37e1b8ac64b7f1f220b
dwidge <dwidge+gh@gmail.com>
2024-08-23 21:21:54 +0200

    Use minio secrets

--- create-secrets.sh
+++ create-secrets.sh
@@ -10,7 +10,13 @@ read -p "Enter your GitHub Username: " GITHUB_USER
 # Prompt for GitHub Access Token
 read -p "Enter your GitHub Access Token: " GITHUB_TOKEN
 
-# Create or update the Kubernetes secret yaml
+# Prompt for Minio Root User
+read -p "Enter your Minio Root User: " MINIO_ROOT_USER
+
+# Prompt for Minio Root Password
+read -p "Enter your Minio Root Password: " MINIO_ROOT_PASSWORD
+
+# Create or update the Kubernetes secret yaml for GitHub
 cat <<EOF > deploy/secrets/github-registry-secret.yaml
 apiVersion: v1
 kind: Secret
@@ -22,4 +28,17 @@ data:
   .dockerconfigjson: $(echo -n "{\"auths\":{\"ghcr.io\":{\"username\":\"$GITHUB_USER\",\"password\":\"$GITHUB_TOKEN\"}}}" | base64 | tr -d '\n')
 EOF
 
-echo "github-registry-secret created or updated successfully."
+# Create or update the Kubernetes secret yaml for Minio
+cat <<EOF > deploy/secrets/minio-secret.yaml
+apiVersion: v1
+kind: Secret
+metadata:
+  name: minio-secret
+  namespace: default
+type: Opaque
+data:
+  MINIO_ROOT_USER: $(echo -n "$MINIO_ROOT_USER" | base64 | tr -d '\n')
+  MINIO_ROOT_PASSWORD: $(echo -n "$MINIO_ROOT_PASSWORD" | base64 | tr -d '\n')
+EOF
+
+echo "github-registry-secret and minio-secret created or updated successfully."
